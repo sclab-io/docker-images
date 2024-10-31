b6eaeae1a8108dc2db775bae5dfbac7f2a06a13a
SUNGYONG JANG <jsy@adain.kr>
2024-10-31 17:48:59 +0900

    [HA] add qdrant service api key

--- ha/setup.sh
+++ ha/setup.sh
@@ -2,5 +2,8 @@
 
 find . -type f -exec sed -i 's/ip1/0.0.0.0/g' {} +
 find . -type f -exec sed -i 's/ip2/0.0.0.0/g' {} +
+find . -type f -exec sed -i 's|your-domain.com|real-domain.com|g' {} +
 find . -type f -exec sed -i 's|https://your-domain.com|https://real-domain.com|g' {} +
-
+find . -type f -exec sed -i 's|changeThisQdrantApiKey|changeThisQdrantApiKey|g' {} +
+find . -type f -exec sed -i 's|changeThisRedisPassword|changeThisRedisPassword|g' {} +
+find . -type f -exec sed -i 's|changeThisMongoPassword|changeThisMongoPassword|g' {} +
