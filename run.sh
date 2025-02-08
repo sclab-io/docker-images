c5470dab932851a522e59168175ea7f2b9a63bba
SUNGYONG JANG <jsy@adain.kr>
2025-02-08 18:21:03 +0900

    [script] update run.sh (add --remove-orphans)

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 873379329511.dkr.ecr.ap-northeast-2.amazonaws.com
 
 export METEOR_SETTINGS=$(cat ./settings.json)
-docker compose up -d
+docker compose up -d --remove-orphans
