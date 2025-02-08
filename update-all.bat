b3cd2134467407fec39704b46db33fbf9156055e
SungYong Jang <jsy@adain.kr>
2025-02-08 18:49:19 +0900

    update bat files

--- update-all.bat
+++ update-all.bat
@@ -1,8 +1,4 @@
 @echo off
-powershell -NoProfile -Command "$env:METEOR_SETTINGS = ((Get-Content -Raw -Path 'settings.json') -replace '(\r?\n)+',' ').Trim(); docker compose up -d"
-
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 873379329511.dkr.ecr.ap-northeast-2.amazonaws.com
 
-docker compose pull
-docker compose down
-docker compose up -d
+powershell -NoProfile -Command "$env:METEOR_SETTINGS = ((Get-Content -Raw -Path 'settings.json') -replace '(\r?\n)+',' ').Trim(); docker compose pull; docker compose down; docker compose up -d"
--- update.bat
+++ update.bat
@@ -1,6 +1,4 @@
 @echo off
-powershell -NoProfile -Command "$env:METEOR_SETTINGS = ((Get-Content -Raw -Path 'settings.json') -replace '(\r?\n)+',' ').Trim(); docker compose up -d"
-
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 873379329511.dkr.ecr.ap-northeast-2.amazonaws.com
 
 setlocal enabledelayedexpansion
@@ -17,7 +15,7 @@ REM 추출된 컨테이너 ID 출력 (디버그용)
 echo Previous container: !PREVIOUS_CONTAINER!
 
 REM 3. webapp 서비스를 스케일업(컨테이너 2개 실행, 재생성 없이)
-docker compose up -d --no-deps --scale webapp=2 --no-recreate webapp
+powershell -NoProfile -Command "$env:METEOR_SETTINGS = ((Get-Content -Raw -Path 'settings.json') -replace '(\r?\n)+',' ').Trim(); docker compose up -d --no-deps --scale webapp=2 --no-recreate webapp"
 
 echo Wait for 30 seconds for the startup
 timeout /t 30 /nobreak >nul
@@ -28,10 +26,10 @@ timeout /t 1 /nobreak >nul
 docker rm -f !PREVIOUS_CONTAINER!
 
 REM 5. webapp 서비스를 스케일다운(컨테이너 1개 실행, 재생성 없이)
-docker compose up -d --no-deps --scale webapp=1 --no-recreate webapp
+powershell -NoProfile -Command "$env:METEOR_SETTINGS = ((Get-Content -Raw -Path 'settings.json') -replace '(\r?\n)+',' ').Trim(); docker compose up -d --no-deps --scale webapp=1 --no-recreate webapp"
 
 REM 6. sclab-proxy 서비스 중지 후 재빌드하여 실행
 docker compose stop sclab-proxy 
-docker compose up -d --no-deps --build sclab-proxy
+powershell -NoProfile -Command "$env:METEOR_SETTINGS = ((Get-Content -Raw -Path 'settings.json') -replace '(\r?\n)+',' ').Trim(); docker compose up -d --no-deps --build sclab-proxy"
 
 endlocal
