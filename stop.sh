a6ebf0ed676eaeed178a12014dfb2ad93adc2b1c
SUNGYONG JANG <jsy@adain.kr>
2026-03-25 09:30:57 +0900

    [script] update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 export METEOR_SETTINGS=$(cat ./settings.json)
+mkdir -p ./data/logs/
 docker compose logs > ./data/logs/sclab-compose-logs-$(date +%F_%H%M).log
 docker compose stop
